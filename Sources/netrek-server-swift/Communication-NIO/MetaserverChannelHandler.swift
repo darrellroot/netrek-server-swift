bcd9b84a355a97cd256517ecd6ecfdcf83bdf249
Darrell Root <feedback@networkmom.net>
2020-07-13 23:42:44 -0700

    Advertises metaserver info on port 2591

--- Sources/netrek-server-swift/Communication-NIO/MetaserverChannelHandler.swift
+++ Sources/netrek-server-swift/Communication-NIO/MetaserverChannelHandler.swift
@@ -18,11 +18,12 @@ final class MetaserverChannelHandler: ChannelInboundHandler {
     }
     public func channelActive(context: ChannelHandlerContext) {
         let remoteAddress = context.remoteAddress!
+        let message = makeReport().data(using: .utf8)!
         let channel = context.channel
         debugPrint("New metaserver connection from \(remoteAddress)")
-        let message = "Hello world".data(using: .utf8)!
         let buffer = context.channel.allocator.buffer(bytes: message)
         context.channel.writeAndFlush(buffer)
+        context.close(promise: nil)
     }
     public func channelInactive(context: ChannelHandlerContext) {
         let channel = context.channel
@@ -42,4 +43,41 @@ final class MetaserverChannelHandler: ChannelInboundHandler {
         }
         context.close(promise: nil)
     }
+    func makeReport() -> String {
+        let fakeUser = User(name: "unknown", password: "", userinfo: "unknown")
+        let header = """
+<>=======================================================================<>
+  Pl: Rank       Name             Login      Host name                Type
+<>=======================================================================<>\n
+"""
+        let spacer = """
+<>=======================================================================<>\n
+"""
+        
+        var returnValue: String = header
+        for player in universe.players.filter( {$0.status != .free}) {
+            let user = player.user ?? fakeUser
+            let name = user.name.padding(toLength: 17, withPad: " ", startingAt: 0)
+            let login = user.userinfo.padding(toLength: 9, withPad: " ", startingAt: 0)
+            let ship = player.ship.description
+            let hostname = "redacted".padding(toLength: 15, withPad: " ", startingAt: 0)
+            let line = "  \(player.team.letter)\(player.slot.hex): Ensign     \(name) \(login) \(hostname)           \(ship)\n"
+            returnValue += line
+        }
+        returnValue += spacer
+        let fedCount = universe.players.filter({$0.team == .federation && $0.status != .free}).count
+        let romCount = universe.players.filter({$0.team == .roman && $0.status != .free}).count
+        let kazCount = universe.players.filter({$0.team == .kazari && $0.status != .free}).count
+        let oriCount = universe.players.filter({$0.team == .orion && $0.status != .free}).count
+        let fedString = String(format: "%2d",fedCount)
+        let romString = String(format: "%2d",romCount)
+        let kazString = String(format: "%2d",kazCount)
+        let oriString = String(format: "%2d",oriCount)
+        var countLine = "  Feds:\(fedString)   Roms:\(romString)   Kli:\(kazString)   Ori:\(oriString)\n"
+        returnValue += countLine
+        returnValue += "  No wait queue.\n"
+        returnValue += spacer
+        return returnValue
+    }
+
 }
